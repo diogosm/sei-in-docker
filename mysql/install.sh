01a8a3dd2c0c67fd0d7936164c245c4645cfe7fd
diogo <diogosoaresm@ufam.edu.br>
2018-04-23 01:00:39 -0400

    add jod memcache e mysql

---
+++ mysql/install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+rm -rf /var/lib/mysql/*
+chown -R mysql:mysql /var/lib/mysql
+mysql_install_db --user=mysql --datadir="/var/lib/mysql" --rpm
+
+mysqld_safe /usr/bin/mysqld_safe --bind-address=0.0.0.0
+
+# Criação dos bancos de dados do sistema
+mysqladmin create sip
+mysqladmin create sei
+
+# Criação dos usuários utilizados na conexão com SEI e SIP
+mysql -e "CREATE USER 'sip_user'@'%' IDENTIFIED BY 'sip_user'" sip
+mysql -e "CREATE USER 'sei_user'@'%' IDENTIFIED BY 'sei_user'" sei
+mysql -e "GRANT ALL PRIVILEGES ON sip.* TO 'sip_user'@'%'" sip
+mysql -e "GRANT ALL PRIVILEGES ON sei.* TO 'sei_user'@'%'" sei
+
+# Restauração dos bancos de dados
+mysql sei < /tmp/sei_3_0_0_BD_Ref_Exec.sql
+mysql sip < /tmp/sip_3_0_0_BD_Ref_Exec.sql
+
+# Atualização dos parâmetros do SEI e do SIP
+mysql -e "update orgao set sigla='ABC', descricao='ORGAO ABC' where id_orgao=0;" sip
+mysql -e "update sistema set pagina_inicial='http://localhost/sip' where sigla='SIP';" sip
+mysql -e "update sistema set pagina_inicial='http://localhost/sei/inicializar.php', web_service='http://localhost/sei/controlador_ws.php?servico=sip' where sigla='SEI';" sip
+mysql -e "update orgao set sigla='ABC', descricao='ORGAO ABC' where id_orgao=0;" sei
+
+# Remove registros de auditoria presentes na base de referência
+mysql -e "delete from auditoria_protocolo;" sei
+
+# Configuração para utilizar autenticação nativa do SEI/SIP
+mysql -e "update orgao set sin_autenticar='N' where id_orgao=0;" sip
+
+# Atribuição de permissões de acesso externo para o usuário root, senha root
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION;"
+
+exit 0
